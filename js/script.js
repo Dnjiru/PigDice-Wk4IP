dd1215a1b2201e0f14e998951291d09645031765
Mwaniki <mr.mwaniki@gmail.com>
2019-04-12 12:09:02 +0300

    Code for activating and deactivating players panels

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Global Variables
+var player1, var player2;
+
+//Back-End Logic
+//Constructor function for a Player
+function Player(name, turnTotal, diceRoll,overallScore,active) {
+  this.name = name;
+  this.diceRoll = 0;
+  this.turnTotal = 0;
+  this.overallScore = 0;
+  this.active = active;
+}
+
+//Function to disable and enable gaming areas according to which player is active.
+function activeUser() {
+  if (player1.active === true && player2.active === false) {
+    $('.player1Area').children().prop('disabled',false);
+    $('.player1Area').removeClass('disableGamingArea');
+    $('.player2Area').children().prop('disabled', true);
+    $('.player2Area').addClass('disableGamingArea');
+  } else {
+    $('.player1Area').children()prop('disabled' true);
+    $('.player1Area').addClass('disableGaming');
+    $('.player2Area').children(prop('disabled', false));
+    $('.player2Area').removeClass('disableGaming');
+  }
+}
