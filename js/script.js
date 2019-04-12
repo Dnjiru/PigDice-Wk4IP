1c62e20dae15228f034008a2a042453b45c067b9
Mwaniki <mr.mwaniki@gmail.com>
2019-04-12 13:25:06 +0300

    Condition if Dice is 1 alert Your turn is over

--- js/script.js
+++ js/script.js
@@ -24,4 +24,39 @@ function activeUser() {
     $('.player2Area').children(prop('disabled', false));
     $('.player2Area').removeClass('disableGaming');
   }
+};
+
+//Function on What happen when the dice is rolled.
+Player.prototype.roll = function () {
+  var randomNo = Math.floor((Math.random() * 6) + 1); // Random Number Generator from 1-6.
+  this.diceRoll = randomNo;
+  activeUser();
+  if (randomNo === 1) {
+    this.turnTotal = 0;
+    this.diceRoll = 1;
+    //Disable and Enable gaming areas when Dice is 1 according to which player is active.
+    if (this.active === player1.active) { 
+      player1.active = false;
+      player2.active = true;
+      $('.player1Area').children().prop ('disabled', true);
+      $('.player1Area').addClass('disabledGamingArea');
+      $('.player2Area').children().prop('disabled', false);
+      $('.player2Area').removeClass('disableGamingArea');
+
+    } else if (this.active === player2active) {
+      player2.active = false;
+      player1.active = true;
+      $('.player2Area').children().prop('disabled', true);
+      $('.player2Area').addClass('disableGamingArea');
+      $('.player1Area').children().prop('disabled', false);
+      $('.player1Area').removeClass('disableGamingArea');
+    } else {
+      console.log("not working");
+    }
+    return alert ("Oops you got a 1. Your turn is over!");
+  } else {
+    this.turnTotal += randomNo;
+  };
+  return this.diceRoll;
 }
+
